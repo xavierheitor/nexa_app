e9397404a76c80a2c7a399cf26096d76a74f1bce
Heitor Xavier <xavierheitor@hotmail.com>
2025-10-03 19:26:02 -0300

    implementados repos dos veiculos

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -5,32 +5,8 @@ abstract class ApiConstants {
   static const login = '/auth/login';
   static const refreshToken = '/auth/refresh';
 
-  static const tipoAtividade = '/atividade/tipo-atividades';
-  static const atividades = '/atividade';
 
-  static const gruposDefeito = '/defeito/grupo-defeito';
-  static const subgruposDefeito = '/defeito/subgrupo-defeito';
-  static const gruposDefeitoCodigo = '/defeito/grupo-defeito-codigo';
-  static const defeitos = '/defeito';
+  static const veiculos = '/veiculo';
+  static const tiposVeiculo = '/tipo-veiculo';
 
-  static const equipamentos = '/equipamento';
-
-  static const aprs = '/apr/modelos';
-  static const aprPerguntas = '/apr/perguntas';
-  static const aprPerguntasRelacionamentos = '/apr/perguntas-relacionamento';
-  static const aprTipoAtividade = '/apr/modelo-tipo-atividade';
-
-  static const checklist = '/checklist/modelos';
-  static const checklistPerguntas = '/checklist/perguntas';
-  static const checklistPerguntasRelacionamentos =
-      '/checklist/perguntas-relacionamento';
-  static const checklistTipoAtividade = '/checklist/modelo-tipo-atividade';
-
-  static const tecnicos = '/tecnico';
-
-  //*upload endpoints
-  static const uploadAtividade =
-      '/atividade/upload'; // Endpoint para upload de atividades
-
-  // ... adicione mais endpoints conforme necessário
 }
